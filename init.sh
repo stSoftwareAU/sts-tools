d9aa7c7c491c7aafc45312d7c386b348e9e8db05
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-08-19 18:40:46 +1000

    remove temp image after published

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ set -e
 BASE_DIR="$( cd -P "$( dirname "$BASH_SOURCE" )" && pwd -P )"
 cd "${BASE_DIR}"
 
-export TOOLS_VERSION="2.6"
+export TOOLS_VERSION="2.7"
 
 function compareVersion () {
     if [[ $1 == $2 ]]
