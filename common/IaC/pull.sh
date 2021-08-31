a390f61994e43975192d4ffd458c18759f4164c7
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-08-31 10:33:08 +1000

    diagnosing missing tags

--- common/IaC/pull.sh
+++ common/IaC/pull.sh
@@ -17,11 +17,10 @@ ECR="${DOCKER_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 aws ecr get-login-password | docker login --username AWS --password-stdin ${ECR}
 
 if [[ ! -z "${COMMIT_ID}" ]]; then
-  DOCKER_TAG="${COMMIT_ID}"
-
-  docker pull --quiet ${ECR}/temp-${AREA,,}/${DOCKER_REPO}:${COMMIT_ID}
-  docker tag ${ECR}/temp-${AREA,,}/${DOCKER_REPO}:${COMMIT_ID} ${DOCKER_REPO}:latest
+  docker pull --quiet "${ECR}/temp-${AREA,,}/${DOCKER_REPO}:git_${COMMIT_ID}"
+  docker tag "${ECR}/temp-${AREA,,}/${DOCKER_REPO}:git_${COMMIT_ID}" ${DOCKER_REPO}:latest
 else
+  echo "No COMMIT_ID using latest"
   DOCKER_URI="${ECR}/${DOCKER_REPO}"
   docker pull --quiet ${ECR}/${AREA,,}/${DOCKER_REPO}:latest
   docker tag ${ECR}/${AREA,,}/${DOCKER_REPO}:latest ${DOCKER_REPO}:latest
