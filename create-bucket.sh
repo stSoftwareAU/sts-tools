dda4b4e87dc3d7a477cc7cf7d58b4e765c873735
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-09-05 19:44:12 +1000

    code clean up

--- create-bucket.sh
+++ create-bucket.sh
@@ -5,7 +5,9 @@ cd "${BASE_DIR}"
 
 echo "Creating bucket ${S3_BUCKET}"
 aws s3api create-bucket --bucket ${S3_BUCKET} --acl private --region ${REGION} --create-bucket-configuration LocationConstraint=${REGION}
+
 aws s3api put-public-access-block --bucket ${S3_BUCKET} \
     --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
+
 aws s3api put-bucket-versioning --bucket ${S3_BUCKET} \
         --versioning-configuration Status=Enabled
