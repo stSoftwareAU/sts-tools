dc37f39e3d8a0352737f4d59dc544a4601aa72d9
Nigel Leck <Nigel.Leck@dta.gov.au>
2021-09-10 15:06:31 +1000

    Bummped tools version

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ set -e
 BASE_DIR="$(cd -P "$(dirname "$BASH_SOURCE")" && pwd -P)"
 cd "${BASE_DIR}"
 
-export TOOLS_VERSION="3.3"
+export TOOLS_VERSION="3.3.1"
 
 function compareVersion() {
   if [[ $1 == $2 ]]; then
