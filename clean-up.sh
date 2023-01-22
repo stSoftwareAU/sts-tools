c0f27167c65559abd9c5a0c5431658c2d1a7be50
Nigel@SudioMac <nigel@stSoftware.com.au>
2023-01-22 14:17:34 +1100

    fixed on mac

--- clean-up.sh
+++ clean-up.sh
@@ -13,12 +13,12 @@ fi
 ECR="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 
 aws --profile "${PROFILE}" ecr get-login-password | docker login --username AWS --password-stdin ${ECR}
-
-aws --profile "${PROFILE}" ecr describe-repositories --repository-names "${AREA,,}/${DOCKER_REPO}" ||
-  aws --profile "${PROFILE}" ecr create-repository --image-scanning-configuration scanOnPush=true --repository-name "${AREA,,}/${DOCKER_REPO}"
+AREA=$(echo "${AREA}" | tr '[:upper:]' '[:lower:]')
+aws --profile "${PROFILE}" ecr describe-repositories --repository-names "${AREA}/${DOCKER_REPO}" ||
+  aws --profile "${PROFILE}" ecr create-repository --image-scanning-configuration scanOnPush=true --repository-name "${AREA}/${DOCKER_REPO}"
 
 aws --profile "${PROFILE}" ecr batch-delete-image \
-  --repository-name "temp-${AREA,,}/${DOCKER_REPO}" \
+  --repository-name "temp-${AREA}/${DOCKER_REPO}" \
   --image-ids imageTag="git_${GIT_COMMIT}"
 
 #set +e
