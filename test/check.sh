0eaf89c34b68c78fb1be41422fd46c88eec4d461
Nigel (MacBook) <nigel@laptop>
2023-01-24 20:34:04 +1100

    standards

--- test/check.sh
+++ test/check.sh
@@ -3,10 +3,10 @@ set -e
 BASE_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 cd "${BASE_DIR}"
 
-../common/IaC/cve-scan.sh --scan $(pwd)/cve-scan.json --allow $(pwd)/cve-allow.json
+../common/IaC/cve-scan.sh --scan "$(pwd)/cve-scan.json" --allow "$(pwd)/cve-allow.json"
 
 set +e
-../common/IaC/cve-scan.sh --scan $(pwd)/fail-scan.json --allow $(pwd)/cve-allow.json
+../common/IaC/cve-scan.sh --scan "$(pwd)/fail-scan.json" --allow "$(pwd)/cve-allow.json"
 ERROR=$?
 
 if [[ ${ERROR} == 0 ]]; then
